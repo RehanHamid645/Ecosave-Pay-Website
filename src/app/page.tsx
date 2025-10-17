d5355366396c5b3c852f1e9ebb5e03c16f341e7f
RehanHamid645 <rehan.hamid@ecosavegasandpower.com>
2025-10-17 14:58:19 +0100

    blog metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
-
-
-
 import { metadatas } from './metadata'
-
-import HeroSection from '@/components/features/HeroSection'
-import ServiceGrid from '@/components/features/ServiceGrid'
-import TrustSection from '@/components/features/TrustSection'
-import PartnerSection from '@/components/features/PartnerSection'
-import StatsCounter from '@/components/features/StatsCounter'
-import ContactSection from '@/components/features/ContactSection'
+import HomePageClient from './HomePageClient'
 
 
 
 export const metadata = metadatas;
 
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <HeroSection />
-      <PartnerSection />
-      <ServiceGrid />
-      <TrustSection />
-      <StatsCounter />
-      <ContactSection />
-    </main>
-  )
+export default function HomePage() {
+  return <HomePageClient />
+
 }
+
